f37d1a12ead340aebfd2957e8b59e25cba0c7e0a
Akshat Sharma <asd@DESKTOP-U60VJ4D>
2024-12-23 22:05:38 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Vite
+          Learn React with Vite and me
         </a>
       </header>
     </div>
